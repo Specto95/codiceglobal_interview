caf1072fc8829e4756e53f314f7bfd0601cf5f29
Enrique Ortiz <enortizlo@gmail.com>
2026-01-05 14:58:42 -0600

    testing back error deploy

--- codiceglobal_back/api/server-with-local.mjs
+++ codiceglobal_back/api/server-with-local.mjs
@@ -1,12 +1,23 @@
+
 import { createApp } from "./index.mjs";
 import { ProductModel } from "./models/product.mjs";
 import { AuthModel } from "./models/auth.mjs";
 
-const app = createApp({
-    productModel: ProductModel,
-    authModel: AuthModel,
-})
+try{
+    console.log('starting back')
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => console.log("API running"));
-export default app;
+    const app = createApp({
+        productModel: ProductModel,
+        authModel: AuthModel,
+    })
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, "0.0.0.0", () => console.log("API running on", PORT));
+}catch(err){
+    console.error('Error starting back', err)
+    process.exit(1)
+}
+
+console.log('back started')
+
+
+// export default app;
